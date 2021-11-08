3cc9c9961636d2751383e0c231f2f4e3dd059158
Tiff-C <deguntiffany@gmail.com>
2021-11-08 17:49:36 +0000

    Create checkout app and models

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6cm_q%9ax0xynyl&nd^4!13#7vounde5l%(y&=t@e5=hh7&90f'
+SECRET_KEY = (
+    'django-insecure-6cm_q%9ax0xynyl&nd^4!13#7vounde5l%(y&=t@e5=hh7&90f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
